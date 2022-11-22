6458b1faf97721eeb308276f9efb54f4c7415a08
rahimtullashabana <rahimtullashaban@gmail.com>
2022-11-22 12:37:45 +0530

    first commit

---
+++ app/views.py
@@ -0,0 +1,5 @@
+from django.shortcuts import render
+from django.http import HttpResponse
+# Create your views here.
+def wish(request):
+    return HttpResponse('happy birthady my dear sista')
---
+++ shabbu/apps.py
@@ -0,0 +1,6 @@
+from django.apps import AppConfig
+
+
+class ShabbuConfig(AppConfig):
+    default_auto_field = 'django.db.models.BigAutoField'
+    name = 'shabbu'
